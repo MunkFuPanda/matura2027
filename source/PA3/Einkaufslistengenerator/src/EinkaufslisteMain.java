52a3d45b23f0415292d59d5fe9f6dcf2350ea392
Schebesta Kilian <20220272@htlwrn.ac.at>
2025-01-21 09:06:36 +0100

    shit

--- source/PA3/Einkaufslistengenerator/src/EinkaufslisteMain.java
+++ source/PA3/Einkaufslistengenerator/src/EinkaufslisteMain.java
@@ -1,7 +1,10 @@
 import javax.swing.*;
 import java.awt.*;
-import java.util.MissingResourceException;
-import java.util.ResourceBundle;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class EinkaufslisteMain extends JFrame{
     private JComboBox languageBox;
@@ -13,28 +16,59 @@ public class EinkaufslisteMain extends JFrame{
     private JButton addToListButton;
     private JTextArea foodText;
 
-    public EinkaufslisteMain() throws HeadlessException {
+    public EinkaufslisteMain() throws HeadlessException, IOException {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setContentPane(main);
-        setTitle("2. praktische Arbeit");
-        setSize(1200, 800);
+        setTitle("Einkaufslistengenerator");
+        setSize(800, 600);
+
+//        // Create a ResourceBundle for the resources folder
+//        ResourceBundle resourceBundle = ResourceBundle.getBundle("resources");
+//
+//        // Read the content of Products.csv and Produkte.csv
+//        Set<String> uniqueWords = readFiles(resourceBundle, "Products.csv", "Produkte.csv");
+//
+//        // Print the unique words
+//        System.out.println("Unique words: " + uniqueWords);
+
+
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Set<String> readFiles(ResourceBundle resourceBundle, String... fileNames) throws IOException {
+        // Create a set to store the unique words
+        Set<String> uniqueWords = new HashSet<>();
+
+        // Iterate over the file names
+        for (String fileName : fileNames) {
+            // Get the input stream for the file
+            InputStream inputStream = resourceBundle.getBaseBundleName().getClass().getClassLoader().getResourceAsStream(fileName);
+
+            // Read the content of the file
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                // Extract the words before the ";" delimiter
+                String[] words = line.split("\\s+")[0].split(";")[0].split("\\s+");
+                uniqueWords.addAll(Arrays.asList(words));
+            }
+            reader.close();
+        }
+
+        return uniqueWords;
+
+
 
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         EinkaufslisteMain eklm = new EinkaufslisteMain();
         eklm.setVisible(true);
-
-        String baseName = "resources.Produkte";
-
-        try
-        {
-            ResourceBundle bundle = ResourceBundle.getBundle( baseName );
-            System.out.println( bundle.getString("Hello") );
-        }
-        catch ( MissingResourceException e ) {
-            System.err.println( e );
-        }
 
     }
 }
