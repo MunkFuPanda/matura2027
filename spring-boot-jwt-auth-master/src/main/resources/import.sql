18f6e56bf54c541a8b2d5325d02be0546af8b866
Spitzer Markus <20220280@htlwrn.ac.at>
2025-03-02 08:00:00 +0100

    add sql and fix table

--- spring-boot-jwt-auth-master/src/main/resources/import.sql
+++ spring-boot-jwt-auth-master/src/main/resources/import.sql
@@ -3,5 +3,8 @@ INSERT INTO User (id, firstname, lastname, username, password, salary, age) VALU
 INSERT INTO User (id, firstname, lastname, username, password, salary, age)  VALUES (-2, 'Tom', 'Asr', 'tom234', '$2a$04$QED4arFwM1AtQWkR3JkQx.hXxeAk/G45NiRd3Q4ElgZdzGYCYKZOW', 7823, 23);
 INSERT INTO User (id, firstname, lastname, username, password, salary, age)  VALUES (-3, 'Adam', 'Psr', 'adam', '$2a$04$WeT1SvJaGjmvQj34QG8VgO9qdXecKOYKEDZtCPeeIBSTxxEhazNla', 4234, 45);
 
+INSERT INTO Product (productid, imagename, price, productName, validfrom, validuntil) VALUES (-1, 'SoftwareSuper.img', 20.2, 'SofwareSuper', '2022-10-10', '2022-10-12');
+INSERT INTO Product (productid, imagename, price, productName, validfrom, validuntil) VALUES (-2, 'WeySoft.img', 31.99, 'WeySoft', '2022-10-1', '2022-10-2');
+INSERT INTO Product (productid, imagename, price, productName, validfrom, validuntil) VALUES (-3, 'TestSoft.img', 25.09, 'TestSoft', '2022-10-3', '2022-10-4');
 --Tipps: 1) Vergib ids mit negativen Zahlen
 --       2) Datum im Format '2022-10-10'
