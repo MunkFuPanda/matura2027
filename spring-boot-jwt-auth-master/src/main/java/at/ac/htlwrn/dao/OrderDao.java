e07194e130123f2441ff4920384746cb6588b43e
Spitzer Markus <20220280@htlwrn.ac.at>
2025-03-06 08:00:00 +0100

    add daos and tests

---
+++ spring-boot-jwt-auth-master/src/main/java/at/ac/htlwrn/dao/OrderDao.java
@@ -0,0 +1,25 @@
+package at.ac.htlwrn.dao;
+
+import at.ac.htlwrn.model.Order;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * DAO for the Order entity.
+ * Extends CrudRepository for basic CRUD operations.
+ */
+@Repository
+public interface OrderDao extends CrudRepository<Order, Long> {
+
+    /**
+     * Custom query to find an Order by clientName.
+     * @param id Name of the client.
+     * @return Optional containing the found Order, if any.
+     */
+    Optional<Order> findById(Long id);
+
+    List<Order> findAll();
+}
