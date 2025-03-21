769e4ab915f4f6f83f4f085a5ad9154903b790e4
Ruixey <ruixey@gmail.com>
2025-03-21 23:01:13 +0100

    start turning into singleton

--- source/PA3/Sudoku/src/main/java/org/example/SudokuGame.java
+++ source/PA3/Sudoku/src/main/java/org/example/SudokuGame.java
@@ -1,19 +1,26 @@
 package org.example;
 
 import javax.swing.*;
-import java.awt.*;
 
 public class SudokuGame extends JFrame {
-
     private JPanel main;
+    private static final SudokuGame instance = new SudokuGame();
 
-    public SudokuGame() {
+    public static SudokuGame getInstance() {
+        return instance;
+    }
+
+    private SudokuGame() {
         super("Sudoku Game");
+
+        System.out.println("SudokuGame constructor");
         this.setContentPane(main);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.pack();
         this.setVisible(true);
     }
 
-
+    public static void main(String[] args) {
+        SudokuGame.getInstance();
+    }
 }
