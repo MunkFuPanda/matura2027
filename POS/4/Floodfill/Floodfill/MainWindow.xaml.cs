e11c19b51542512b704e55841196ff95018a85e2
Ruixey <ruixey@gmail.com>
2025-09-29 08:55:10 +0200

    floodfill copy pasta

--- POS/4/Floodfill/Floodfill/MainWindow.xaml.cs
+++ POS/4/Floodfill/Floodfill/MainWindow.xaml.cs
@@ -1,12 +1,9 @@
-﻿using Microsoft.Win32;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Xml.Serialization;
 
 namespace PixelDraw
 {
