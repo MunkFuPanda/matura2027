f13a40ae52b448ff5effcc14fcd22158ed66608c
Spizza <markus@spizza.cc>
2026-04-08 20:14:16 +0200

    add breast cancer stuff

--- DBI/4/UE14/Strobl.sql
+++ DBI/4/UE14/Strobl.sql
@@ -1,70 +1,108 @@
-use AirportDB;
+USE AirportDB;
 
 -- 1.)
 
-go
-create or alter function dbo.udffindData(@startDate varchar(50), @endDate varchar(50))
-returns table 
-as
-return (
-	select f.Airline_code, Ai.Airline_Name, A.Aircraft_code, f.Flight_number, FlA.Leg_number, PA.Passport_number, PA.First_name, PA.Minit, PA.Last_name, PA.Air_ticket_number from Flight_shedule_date fd
-		join flight f on f.Flight_number = fd.Flight_number
-		join Airline Ai on Ai.Airline_code = f.Airline_code
-		join Aircraft A on A.Airline_code = f.Airline_code
-		join Flight_leg_A FlA on FlA.Flight_number = f.Flight_number
-		join Passenger_A PA on FlA.Leg_number = PA.Leg_number
-		where fd.Date between @startDate and @endDate
+GO
+CREATE OR ALTER FUNCTION dbo.udffindData(@startDate VARCHAR(50), @endDate VARCHAR(50))
+returns TABLE 
+AS
+RETURN (
+	SELECT
+	f.Airline_code
+	,Ai.Airline_Name
+	,A.Aircraft_code
+	,f.Flight_number
+	,FlA.Leg_number
+	,PA.Passport_number
+	,PA.First_name
+	,PA.Minit
+	,PA.Last_name
+	,PA.Air_ticket_number
+FROM
+	Flight_shedule_date fd
+	JOIN flight f ON f.Flight_number = fd.Flight_number
+	JOIN Airline Ai ON Ai.Airline_code = f.Airline_code
+	JOIN Aircraft A ON A.Airline_code = f.Airline_code
+	JOIN Flight_leg_A FlA ON FlA.Flight_number = f.Flight_number
+	JOIN Passenger_A PA ON FlA.Leg_number = PA.Leg_number
+WHERE fd.Date BETWEEN @startDate AND @endDate
 	)
-go
+GO
 
-select * from udffindData('2018-12-21', '2018-12-21')
+SELECT
+	*
+FROM
+	udffindData('2018-12-21', '2018-12-21')
 
 -- 2.)
 
-go
-create or alter function dbo.statusInfo(@status varchar(50), @date varchar(50))
-returns table
-as
-return (
-	select FlB.Status, FlB.Remark, FlB.Arrival_teminal_number, FlA.Staff_ID, FlA.Leg_number, FlA.Flight_number, PA.Passport_number, 
-	concat(PA.First_Name, ' ', PA.Last_name) as Passenger_name, PC.Passenger_catogary, PR.Requirement, fd.Date from Flight_shedule_date fd
-		join flight f on f.Flight_number = fd.Flight_number
-		join Airline Ai on Ai.Airline_code = f.Airline_code
-		join Aircraft A on A.Airline_code = f.Airline_code
-		join Flight_leg_A FlA on FlA.Flight_number = f.Flight_number
-		join Passenger_A PA on FlA.Leg_number = PA.Leg_number
-		join Flight_leg_B FlB on FlB.Arrival_teminal_number = FlA.Arrival_teminal_number
-		join Passenger_catogary PC on PC.Passport_number = PA.Passport_number
-		join Passenger_requirements PR on PR.Passport_number = PA.Passport_number
-		where fd.Date = @date
-		and flB.Status = @status
+GO
+CREATE OR ALTER FUNCTION dbo.statusInfo(@status VARCHAR(50), @date VARCHAR(50))
+returns TABLE
+AS
+RETURN (
+	SELECT
+	FlB.Status
+	,FlB.Remark
+	,FlB.Arrival_teminal_number
+	,FlA.Staff_ID
+	,FlA.Leg_number
+	,FlA.Flight_number
+	,PA.Passport_number
+	,concat(PA.First_Name, ' ', PA.Last_name) AS Passenger_name
+	,PC.Passenger_catogary
+	,PR.Requirement
+	,fd.Date
+FROM
+	Flight_shedule_date fd
+	JOIN flight f ON f.Flight_number = fd.Flight_number
+	JOIN Airline Ai ON Ai.Airline_code = f.Airline_code
+	JOIN Aircraft A ON A.Airline_code = f.Airline_code
+	JOIN Flight_leg_A FlA ON FlA.Flight_number = f.Flight_number
+	JOIN Passenger_A PA ON FlA.Leg_number = PA.Leg_number
+	JOIN Flight_leg_B FlB ON FlB.Arrival_teminal_number = FlA.Arrival_teminal_number
+	JOIN Passenger_catogary PC ON PC.Passport_number = PA.Passport_number
+	JOIN Passenger_requirements PR ON PR.Passport_number = PA.Passport_number
+WHERE fd.Date = @date
+	AND flB.Status = @status
 	)
-go
+GO
 
-select * from dbo.statusInfo('Canceled', '2018-12-21')	
+SELECT
+	*
+FROM
+	dbo.statusInfo('Canceled', '2018-12-21')	
 
 -- 3.)
 
 
-go
-create or alter procedure dbo.stp_passExpire
-@PassportNumber varchar(50)
-as
-begin 
-	if ((select Date_of_Expire from Passenger_A where Passport_number = @PassportNumber) < cast(GETDATE() as date))
-	begin
-		print 'Passengers Passport has been expired'
-	end
-	else if exists (select Date_of_Expire from Passenger_A where Passport_number = @PassportNumber)
-	begin
-		print 'This Passenger has a valid Passport'
-	end
-	else
-	begin
-		print 'This Passport Number does not exist'
-	end
-end
-go
+GO
+CREATE OR ALTER PROCEDURE dbo.stp_passExpire
+	@PassportNumber VARCHAR(50)
+AS
+BEGIN
+	IF ((SELECT
+		Date_of_Expire
+	FROM
+		Passenger_A
+	WHERE Passport_number = @PassportNumber) < cast(GETDATE() AS DATE))
+	BEGIN
+		PRINT 'Passengers Passport has been expired'
+	END
+	ELSE IF EXISTS (SELECT
+		Date_of_Expire
+	FROM
+		Passenger_A
+	WHERE Passport_number = @PassportNumber)
+	BEGIN
+		PRINT 'This Passenger has a valid Passport'
+	END
+	ELSE
+	BEGIN
+		PRINT 'This Passport Number does not exist'
+	END
+END
+GO
 
-exec dbo.stp_passExpire 'M100123155'
-exec dbo.stp_passExpire 'M100123464'
+EXEC dbo.stp_passExpire 'M100123155'
+EXEC dbo.stp_passExpire 'M100123464'
